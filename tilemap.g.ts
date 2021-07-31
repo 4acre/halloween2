27896020fed0bc1205acdf9b2d987b547574acb1
4acre <88250011+4acre@users.noreply.github.com>
2021-07-31 14:46:30 -0400

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,89 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level":
+            case "level":return tiles.createTilemap(hex`1e001e00050e0303010303030303030103030d0303030e010e0e0e0e0e0e010e0e04060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b07060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b07060b0b11111111111111111212121212120b0b11121111111111110b0b070c0b0b11111111121111111211121212120b0b11121212121212110b0b070a0b0b111111140b0b0b0b0b0b0b0b12120b0b11121211121211110b0b07060b0b1212110b0b0b0b0b0b0b0b0b12120b0b11111212121111110b0b07060b0b121111111112111211120b0b12120b0b11111212121211110b0b07060b0b121212121212121212120b0b12120b0b11111212121211110b0b07060b0b0b0b0b0b0b0b0b1212120b0b11120b0b11121211121112110b0b07060b0b0b0b0b0b0b0b0b1212110b0b12120b0b11121211121212110b0b0706111111111111110b0b1212120b0b12110b0b11121111121212110b0b070a111111111111110b0b1211120b0b12110b0b11121111111212110b0b0706111111111111110b0b1211120b0b12120b0b11121111111212110b0b070c111111111111110b0b1111120b0b11120b0b11121211111211110b0b0706111111111111110b0b1211120b0b12110b0b11121212121111110b0b07060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b07060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b070a0b1212121212120b0b1211121212121212111212121212121211121207060b1111111111120b0b1111111112111211111111111111111111111107060b1212121211120b0b1212111111111111121111111112121211121107060b1212121212120b0b1212111211111111121112121212121112121107060b1211121112110b0b1212111211111112121111111111111112111107060b1211121212110b0b1212111211111112121211111211121111111207060b1211111212120b0b12111112121111121211121211121211111211070a0b1211111111120b0b121212121212111111121212111111121212120706111211111212120b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b07060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b07100b0b0b0b0b0b0b0b0b121112111111121111121111111211110b0b0f07090202020213020202020202020202020202020202020202020202020208`, img`
+222222222222222222222222222222
+2............................2
+2............................2
+2..........2.................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+2..............2...........2.2
+2............................2
+2.................2..........2
+2............................2
+2............................2
+2............................2
+2............................2
+2............................2
+............................22
+222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest2,sprites.dungeon.floorLight0,sprites.dungeon.purpleOuterWest1,myTiles.tile11,sprites.dungeon.purpleOuterNorth0,sprites.castle.rock1,sprites.dungeon.stairWest,sprites.dungeon.hazardLava1,sprites.dungeon.hazardLava0,sprites.dungeon.purpleOuterSouth2,sprites.dungeon.chestClosed], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "tile3":return tile3;
+            case "tile4":return tile4;
+            case "tile5":return tile5;
+            case "tile6":return tile6;
+            case "tile7":return tile7;
+            case "tile8":return tile8;
+            case "tile9":return tile9;
+            case "tile10":return tile10;
+            case "tile11":return tile11;
+            case "tile12":return tile12;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
